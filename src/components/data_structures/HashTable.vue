ff2da17275ce3e0e176aa6d16d19e7329fd4089f
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-02-24 13:45:42 -0500

    Add initial hashtable

--- src/components/data_structures/HashTable.vue
+++ src/components/data_structures/HashTable.vue
@@ -1,3 +1,67 @@
+<script>
+import HashTable from "../../model/HashTable";
+
+export default {
+    data() {
+        return {
+            size: 10,
+            key: null,
+            value: null,
+            hashTable: new HashTable(10),
+        };
+    },
+    methods: {
+        set() {
+            if (this.key && this.value) {
+                this.hashTable.set(this.key, this.value);
+            } else {
+                alert("Enter both key and value.");
+            }
+        },
+    },
+};
+</script>
+
 <template>
-    <h3>HashTable</h3>
+    <div id="hashtable">
+        <div>
+            <input type="text" placeholder="Key" v-model="key" />
+            <input type="text" placeholder="Value" v-model="value" />
+            <button @click="set">Set</button>
+        </div>
+        <div id="hashtable-container">
+            <div
+                class="hashtable-entry"
+                v-for="(entryValue, index) in hashTable.data"
+                :key="index"
+            >
+                <div class="entry-key">
+                    {{ index }}
+                </div>
+                <div class="entry-value">
+                    {{ entryValue ? entryValue : "None" }}
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+#hashtable {
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+}
+#hashtable-container {
+    padding-top: 2rem;
+    border-bottom: solid 1px whitesmoke;
+}
+.hashtable-entry {
+    border: solid 1px whitesmoke;
+    border-bottom: 0;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    padding: 0.5rem;
+}
+.entry-key {
+    border-right: solid 1px whitesmoke;
+}
+</style>
---
+++ src/model/HashTable.js
@@ -0,0 +1,27 @@
+export default class HashTable {
+    constructor(size) {
+        this.data = size <= 0 ? [] : new Array(size);
+        this.size = size;
+        this.lastHash = null;
+    }
+
+    _generateHash(key) {
+        let intHash = 0;
+        for (let charIndex = 0; charIndex < key.length; charIndex++) {
+            intHash =
+                (intHash + key.charCodeAt(charIndex) * charIndex) % this.size;
+        }
+        this.lastHash = intHash;
+        return intHash;
+    }
+
+    set(key, value) {
+        const hashedIndex = this._generateHash(key);
+        console.log(hashedIndex);
+        this.data[hashedIndex] = value;
+    }
+
+    get(key) {
+        return this.data[key];
+    }
+}
